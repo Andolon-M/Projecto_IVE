d29217ac6e4f0ef47e75be42986fa6c2a4cbec2f
Andolon-M <aemendez-l@outlook.com>
2023-11-27 14:09:51 -0500

    correccion resources css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <!-- Scripts -->    
-    @vite(['public/css/app.css', 'resources/js/app.js'])
+   
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
